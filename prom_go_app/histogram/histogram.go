236ea6b9775f033e6098d8b3a3ba9e29ca627efa
Amit-unchartered <amitkumaragrawal42@gmail.com>
2024-01-28 23:17:52 +0530

    histogram

---
+++ prom_go_app/histogram/histogram.go
@@ -0,0 +1,52 @@
+// calculate average request latency of go application while handling different http requests
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var REQUEST_RESPOND_TIME = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	Name: "go_app_response_latency_seconds",
+	Help: "Response latency in seconds",
+}, []string{"path"})
+
+func routeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start_time := time.Now()
+		route := mux.CurrentRoute(r)
+		path, _ := route.GetPathTemplate()
+
+		next.ServeHTTP(w, r)
+		time_taken := time.Since(start_time)
+		REQUEST_RESPOND_TIME.WithLabelValues(path).Observe(time_taken.Seconds())
+	})
+}
+
+func main() {
+	//start the application
+	startMyApp()
+}
+
+func startMyApp() {
+	router := mux.NewRouter()
+	router.HandleFunc("/birthday/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		greetings := fmt.Sprintf("Happy Birthday %s :)", name)
+		time.Sleep(2 * time.Second)
+		w.Write([]byte(greetings))
+	}).Methods("GET")
+
+	router.Use(routeMiddleware)
+	log.Println("starting the application server...")
+	router.Path("/metrics").Handler(promhttp.Handler())
+	http.ListenAndServe(":8000", router)
+}
